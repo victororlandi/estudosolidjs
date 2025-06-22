41afe06afc3f1949c149f9e66d16651e1115eaaa
victororlandi <victororlandi2016@gmail.com>
2025-06-22 18:05:21 -0300

    primeiro contato com solid js

---
+++ src/componentes/Curso.jsx
@@ -0,0 +1,9 @@
+export default function Curso(props){
+    return (
+        <div class="bg-white p-4 text-center"
+            classList={{"rounded-md": props.rounded, "shadow-md": !props.flat}}
+        >
+            {props.children}
+        </div>
+    )
+}
